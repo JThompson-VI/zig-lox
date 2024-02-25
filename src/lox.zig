37473191eaf76771f201c5b7ed5844f1332268cc
Jacob Thompson <a01657923@usu.edu>
2024-02-24 23:04:52 -0700

    refactor add lox file for run stuffs

---
+++ src/lox.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+const fs = std.fs;
+
+pub fn dbg(comptime x: []const u8) void {
+    std.debug.print(x, .{});
+    std.debug.print("\n", .{});
+}
+var hadError = false;
+
+pub fn run(source: []const u8) void {
+    std.debug.print("{s}", .{source});
+}
+
+pub fn runPrompt(allocator: std.mem.Allocator) !void {
+    const stdin_file = std.io.getStdIn();
+    var buffered_reader = std.io.bufferedReader(stdin_file.reader());
+    const stdin = buffered_reader.reader();
+
+    var line = std.ArrayList(u8).init(allocator);
+    defer line.deinit();
+
+    const writer = line.writer();
+
+    while (stdin.streamUntilDelimiter(writer, '\n', null)) {
+        defer line.clearRetainingCapacity();
+        run(line.items);
+        hadError = false;
+    } else |err| switch (err) {
+        error.EndOfStream => {},
+        else => return err,
+    }
+}
+
+pub fn runFile(allocator: std.mem.Allocator, file: [:0]u8) !void {
+    var open_file = try fs.cwd().openFile(file, .{});
+    defer open_file.close();
+    const file_len = try open_file.getEndPos();
+
+    const buf = try allocator.alloc(u8, file_len);
+    defer allocator.free(buf);
+
+    _ = try open_file.readAll(buf);
+
+    run(buf);
+    if (hadError) std.os.exit(65);
+}
+
+pub fn report(line: i32, where: []const u8, message: []const u8) !void {
+    const stderr = std.io.getStdErr().writer();
+    try stderr.print("[line {d}] Error {s}: {s}\n", .{ line, where, message });
+    hadError = true;
+}
+pub fn print_error(line: i32, message: []const u8) !void {
+    try report(line, "", message);
+}
--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,8 @@
+const scanner = @import("./scanner.zig");
+const lox = @import("./lox.zig");
+
 const std = @import("std");
 const fs = std.fs;
-
-fn dbg(comptime x: []const u8) void {
-    std.debug.print(x, .{});
-    std.debug.print("\n", .{});
-}
-
-fn run(source: []const u8) void {
-    std.debug.print("{s}", .{source});
-}
-
-fn runPrompt(allocator: std.mem.Allocator) !void {
-    const stdin_file = std.io.getStdIn();
-    var buffered_reader = std.io.bufferedReader(stdin_file.reader());
-    const stdin = buffered_reader.reader();
-
-    var line = std.ArrayList(u8).init(allocator);
-    defer line.deinit();
-
-    const writer = line.writer();
-
-    while (stdin.streamUntilDelimiter(writer, '\n', null)) {
-        defer line.clearRetainingCapacity();
-        run(line.items);
-    } else |err| switch (err) {
-        error.EndOfStream => {},
-        else => return err,
-    }
-}
-
-fn runFile(allocator: std.mem.Allocator, file: [:0]u8) !void {
-    var open_file = try fs.cwd().openFile(file, .{});
-    defer open_file.close();
-    const file_len = try open_file.getEndPos();
-
-    const buf = try allocator.alloc(u8, file_len);
-    defer allocator.free(buf);
-
-    _ = try open_file.readAll(buf);
-
-    run(buf);
-}
 
 pub fn main() !u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -50,12 +12,14 @@ pub fn main() !u8 {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
     switch (args.len) {
-        1 => try runPrompt(allocator),
-        2 => try runFile(allocator, args[1]),
+        1 => try lox.runPrompt(allocator),
+        2 => try lox.runFile(allocator, args[1]),
         else => {
             std.debug.print("Usage zig-lox [script]", .{});
             return 64;
         },
     }
+    const foo = scanner.Scanner.init(allocator, ""); // for zig to check my code
+    _ = foo;
     return 0;
 }
